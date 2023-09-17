117d6b8e802f6cc299dcd585b387e14ed6415dd9
Himashree Hazarika <52618743+HIMA10SHREE@users.noreply.github.com>
2023-09-17 07:50:34 +0530

    Add files via upload

---
+++ static_websitehosting_S3/main.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "static_page" {
+  bucket = var.bucketname
+
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.static_page.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# resource "aws_s3_bucket_public_access_block" "example" {
+#   bucket = aws_s3_bucket.static_page.id
+
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.static_page.id
+  key    = "index.html"
+  source = "index.html"
+
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.static_page.id
+  key    = "error.html"
+  source = "error.html"
+
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "static-hosting" {
+  bucket = aws_s3_bucket.static_page.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+
---
+++ static_websitehosting_S3/variables.tf
@@ -0,0 +1,3 @@
+variable "bucketname" {
+  default = "hima-static-page"
+}
