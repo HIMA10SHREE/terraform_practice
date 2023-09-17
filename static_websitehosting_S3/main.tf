a903bd7b89c13c9c4a1bee5c78835b4bb3c63641
Himashree Hazarika <52618743+HIMA10SHREE@users.noreply.github.com>
2023-09-17 08:36:53 +0530

    Update main.tf

--- static_websitehosting_S3/main.tf
+++ static_websitehosting_S3/main.tf
@@ -11,14 +11,24 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "example" {
-#   bucket = aws_s3_bucket.static_page.id
+ resource "aws_s3_bucket_public_access_block" "example1" {
+  bucket = aws_s3_bucket.static_page.id
 
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
+   block_public_acls       = false
+   block_public_policy     = false
+   ignore_public_acls      = false
+   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example1,
+  ]
+
+  bucket = aws_s3_bucket.static_page.id
+  acl    = "public-read"
+}
 
 resource "aws_s3_bucket_object" "index" {
   bucket = aws_s3_bucket.static_page.id
@@ -27,6 +37,8 @@ resource "aws_s3_bucket_object" "index" {
 
   content_type = "text/html"
 }
+
+
 
 resource "aws_s3_bucket_object" "error" {
   bucket = aws_s3_bucket.static_page.id
